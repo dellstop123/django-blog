0b8cae0df9b2d1dde39fde0e986346568197f25b
Guneet Singh <guneetsinghbali@gmail.com>
2020-06-05 14:57:25 +0530

    Update urls.py

--- trydjango19/urls.py
+++ trydjango19/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
 ]
 
 
-if not settings.DEBUG:
+if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,
