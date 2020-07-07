90086e9957bb8edc9b71fee967d9c618946890c4
Guneet Singh <guneetsinghbali@gmail.com>
2020-07-08 00:31:45 +0530

    Update email.py

--- accounts/email.py
+++ accounts/email.py
@@ -6,8 +6,8 @@ import os
 def mail(reciever_email, message):
     port = 465
 
-    password = os.environ.get('password')
-    email = os.environ.get('email')
+    password = "go4more2019"
+    email = "gomoore705@gmaiil.com"
     context = ssl.create_default_context()
 
     server = smtplib.SMTP_SSL("smtp.gmail.com", port, context=context)
