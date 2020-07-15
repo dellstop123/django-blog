1a11595847e868944a7a21f68119b461dac22c3a
Guneet Singh <guneetsinghbali@gmail.com>
2020-07-15 15:59:59 +0530

    Update sitemap.py

--- trydjango19/sitemap.py
+++ trydjango19/sitemap.py
@@ -1,6 +1,6 @@
 from django.contrib.sitemaps import Sitemap
 from django.shortcuts import reverse
-
+from posts.models import Post
 
 class StaticViewSitemap(Sitemap):
     def items(self):
@@ -8,3 +8,9 @@ class StaticViewSitemap(Sitemap):
 
     def location(self, item):
         return reverse(item)
+    
+class SnippetSitemap(Sitemap):
+    def items(self):
+        return Post.objects.all()
+    
+    
