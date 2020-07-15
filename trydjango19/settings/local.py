60b3edff5406b714917865efe03181f9d338d678
Guneet Singh <guneetsinghbali@gmail.com>
2020-07-15 15:39:40 +0530

    Update local.py

--- trydjango19/settings/local.py
+++ trydjango19/settings/local.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
     'payments.apps.PaymentsConfig',
     # third party
     'crispy_forms',
