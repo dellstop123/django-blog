119b7e815603fb86af7da2c67e94d01cfb892116
Guneet Singh <guneetsinghbali@gmail.com>
2020-08-14 15:54:14 +0530

    Update views.py

--- posts/views.py
+++ posts/views.py
@@ -22,9 +22,11 @@ except ImportError:
     from django.core.urlresolvers import reverse
 from newsapi import NewsApiClient
 import redis
+import os
 
+REDIS_URL = os.environ.get('REDIS_URL')
 
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis(host=REDIS_URL, port=6379, db=0)
 # Create your views here.
 
 
