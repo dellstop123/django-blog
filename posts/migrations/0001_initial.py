a50330f1347fc184a70d81156f1b1a18a267d699
Guneet Singh <guneetsinghbali@gmail.com>
2020-09-16 20:08:38 +0530

    Update 0001_initial.py

--- posts/migrations/0001_initial.py
+++ posts/migrations/0001_initial.py
@@ -26,37 +26,6 @@ class Migration(migrations.Migration):
                 ('updated', models.DateTimeField(auto_now=True)),
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
             ],
-        ),
-        migrations.CreateModel(
-            name='Post',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=120)),
-                ('slug', models.SlugField(max_length=255, unique=True)),
-                ('image', models.FileField(storage=gdstorage.storage.GoogleDriveStorage(), upload_to='posts/')),
-                ('height_field', models.IntegerField(default=0)),
-                ('width_field', models.IntegerField(default=0)),
-                ('content', ckeditor.fields.RichTextField()),
-                ('draft', models.BooleanField(default=False)),
-                ('publish', models.DateField()),
-                ('read_time', models.IntegerField(default=0)),
-                ('updated', models.DateTimeField(auto_now=True)),
-                ('timestamp', models.DateTimeField(auto_now_add=True)),
-                ('likes', models.IntegerField(default=0)),
-                ('user', models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-            options={
-                'ordering': ['-timestamp', '-updated'],
-            },
-        ),
-        migrations.CreateModel(
-            name='Images',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('image', models.FileField(storage=gdstorage.storage.GoogleDriveStorage(), upload_to='images/')),
-                ('height_field', models.IntegerField(default=0)),
-                ('width_field', models.IntegerField(default=0)),
-                ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='posts.Post')),
-            ],
-        ),
+        )
+
     ]
